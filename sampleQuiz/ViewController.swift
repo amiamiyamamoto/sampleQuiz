3a195a7b212874469866cf21ce2cd69cf5ace624
AMI <a_yamamoto@innovation.co.jp>
2017-12-28 19:55:55 +0900

    Buttonタップで音が出る

--- sampleQuiz/ViewController.swift
+++ sampleQuiz/ViewController.swift
@@ -7,12 +7,40 @@
 //
 
 import UIKit
+import AudioToolbox
 
 class ViewController: UIViewController {
 
+    //システムサウンドButtonの処理
+    @IBAction func tapSystemSound(_ sender: Any) {
+        AudioServicesPlaySystemSoundWithCompletion(1000){
+            //サウンドが鳴り終わった後に行う処理
+        }
+    }
+    
+    //カスタムサウンドButtonの処理
+    @IBAction func tapCustomSound(_ sender: Any) {
+        //指定するサウンドファイルの再生時間が30秒以下であること
+        let soundUrl = Bundle.main.url(forResource: "button56", withExtension: "mp3")
+        
+        //サウンドIDを指定するための変数を準備
+        var soundId:SystemSoundID = 0
+        
+        //サウンドファイルを登録してサウンドIDを取得
+        AudioServicesCreateSystemSoundID(soundUrl as! CFURL, &soundId)
+        AudioServicesPlaySystemSoundWithCompletion(soundId){
+            
+        }
+    }
+    
+    //バイブレーションButtonの処理
+    @IBAction func tapVibrationSound(_ sender: Any) {
+        AudioServicesPlaySystemSoundWithCompletion(kSystemSoundID_Vibrate){
+            //Vibrationが終わったときにする処理
+        }
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
